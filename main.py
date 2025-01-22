2ef84624225a790b700d420f9df4f745c757764b
Anand Kumar Billakanti <billakanti.anand@gmail.com>
2025-01-22 16:04:35 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ from github_api import GitHubAPI
 if __name__ == "__main__":
     token = "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"  
     github = GitHubAPI(token)
-
-
     github.print_user_details()
 
