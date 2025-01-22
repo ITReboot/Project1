4db8b450ec642f61001d714c3e6940d5265cf985
Anand Kumar Billakanti <billakanti.anand@gmail.com>
2025-01-22 17:00:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# main_script.py
-
 from github_api import GitHubAPI
 
-# Example usage
 if __name__ == "__main__":
     github = GitHubAPI()
 
-    # Fetch and print user details
+    
     github.print_user_details()
